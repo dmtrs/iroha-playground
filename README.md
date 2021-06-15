12be7c2724bafeeba29a274ac9cada090520a96b
Dmtrs Flaco Meng <tydeas.dr@gmail.com>
2021-06-16 00:15:12 +0300

    chore(docker-compose): Adds docker-compose for network and keygen.py script

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# iroha-playground
-Hyperledger Iroha playground project
+# ![logo](./logo.svg) Iroha Playground
+
+
+## Test network
+
+```
+docker-compose up
+```
+
+## Local image
+
+```
+docker build -t iroha-playground
+docker run -v $(pwd):/usr/src/app --rm -ti iroha-playground poetry run pythone keygen.py --name {KEY_NAME_HERE}
+```
---
+++ keygen.py
@@ -0,0 +1,41 @@
+import typing
+import argparse
+import uuid
+
+from dataclasses import dataclass
+
+from iroha import IrohaCrypto
+
+@dataclass
+class Key:
+    value: bytes
+    name: str
+
+def generate_keypair(name: str) -> typing.Iterable[Key]:
+    private_key = Key(
+        name=f'{name}.priv',
+        value=IrohaCrypto.private_key(),
+    )
+    yield private_key
+    yield Key(
+        name=f'{name}.pub',
+        value=IrohaCrypto.derive_public_key(private_key.value),
+    )
+
+def main(*, name: str, **kwargs: typing.Dict[str, str]) -> None:
+    for key in generate_keypair(name):
+        with open(key.name, 'wb') as f:
+            f.write(key.value)
+        print(f'🔒 Wrote key {key.name}')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        argument_default=argparse.SUPPRESS
+    )
+    parser.add_argument(
+        '--name',
+        help='keypair name',
+        required=True,
+    )
+    main(**vars(parser.parse_args()))
